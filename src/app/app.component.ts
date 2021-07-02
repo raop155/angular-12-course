d53a8b2ab3e9f5455bb634d419bdf30f066c6bbb
Ricardo Olarte <raop155@hotmail.com>
2021-07-02 18:00:54 -0500

    Crear un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
 
-  acumular (valor: number) {
-    this.numero += valor ;
-  }
 }
