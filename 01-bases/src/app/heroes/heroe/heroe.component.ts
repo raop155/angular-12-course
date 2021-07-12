da23d72d222f038cef1953e3d0c2df90bee88a98
Ricardo Olarte <raop155@hotmail.com>
2021-07-12 17:49:59 -0500

    Cambios en el template del componente

--- 01-bases/src/app/heroes/heroe/heroe.component.ts
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
 
 })
 export class HeroComponent {
+  nombre: string = "Ironman";
+  edad: number = 45;
 
+  get nombreCapitalizado() {
+    return this.nombre.toUpperCase()
+  }
+
+  obtenerNombre(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
 }
