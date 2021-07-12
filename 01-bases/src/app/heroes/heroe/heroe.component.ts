08871eb2975e1325f58d72f2f0cfae78e391c7e4
Ricardo Olarte <raop155@hotmail.com>
2021-07-12 17:55:07 -0500

    One way data binding

--- 01-bases/src/app/heroes/heroe/heroe.component.ts
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -9,11 +9,19 @@ export class HeroComponent {
   nombre: string = "Ironman";
   edad: number = 45;
 
-  get nombreCapitalizado() {
+  get nombreCapitalizado(): string {
     return this.nombre.toUpperCase()
   }
 
   obtenerNombre(): string {
     return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarNombre(): void {
+    this.nombre = "Spiderman";
+  }
+
+  cambiarEdad(): void {
+    this.edad = 30;
   }
 }
