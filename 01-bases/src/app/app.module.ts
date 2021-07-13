9d6637464a79a579c92684c68e0531067bd075c5
Ricardo Olarte <raop155@hotmail.com>
2021-07-13 15:59:36 -0500

    Modulo DBZ

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppComponent } from './app.component';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     HeroesModule,
     ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
