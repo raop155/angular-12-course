ee1d7fcc5e46c63e978f9a8a2ad682a16d0416ed
Ricardo Olarte <raop155@hotmail.com>
2021-07-13 16:33:10 -0500

    Mostrar DBZ listado de personajes

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -10,13 +10,30 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: "Goku",
+      poder: 1500
+    },
+    {
+      nombre: "Vegeta",
+      poder: 7500
+    }
+  ];
+
   nuevo: Personaje = {
-    nombre: "Trunks",
-    poder: 14000
+    nombre: "",
+    poder: 0
   }
 
   agregar() {
+    if (this.nuevo.nombre.trim().length === 0) return;
     console.log(this.nuevo);
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre: "",
+      poder: 0
+    }
   }
 
 }
