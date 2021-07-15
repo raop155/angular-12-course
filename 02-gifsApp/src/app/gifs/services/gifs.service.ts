2478285c2d7f59d5d3d032e18f2f3250427879d4
Ricardo Olarte <raop155@hotmail.com>
2021-07-15 18:01:38 -0500

    HttpClientModule

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey:string = "7SXSYlppZqoMEPSzskXYhyLN85Y6RnmD";
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
 
   buscarGifs(query: string = ""): void {
     query = query.trim().toLowerCase();
@@ -18,6 +22,14 @@ export class GifsService {
     this._historial.unshift(query);
     this._historial = this.historial.splice(0, 10);
     console.log(this._historial);
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+    .subscribe( (response: any) => {
+      console.log(response.data);
+    })
+
   }
+
+
 }
 
