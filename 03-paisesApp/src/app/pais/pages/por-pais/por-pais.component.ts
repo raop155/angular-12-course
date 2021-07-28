f2103ffea7f3d57f8aa46a5a74d8699b9e4f4c5e
Ricardo Olarte <raop155@hotmail.com>
2021-07-27 19:16:55 -0500

    Manejo de errores

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,14 +10,19 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = ""
+  hayError: boolean = false;
 
   constructor(private paisService:PaisService) { }
 
   buscar() {
+    this.hayError = false
     console.log(this.termino);
     this.paisService.buscarPais(this.termino).subscribe(
       response => {
         console.log(response);
+      }, error => {
+        console.log(error);
+        this.hayError = true
       }
     )
   }
