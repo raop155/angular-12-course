0fe04b86617bf3edb23f3d32b23e93afe95881f0
Ricardo Olarte <raop155@hotmail.com>
2021-07-27 19:43:18 -0500

    Populate Country Table

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent {
 
   termino: string = ""
   hayError: boolean = false;
+  paises: Country[] = []
 
   constructor(private paisService:PaisService) { }
 
@@ -20,9 +22,11 @@ export class PorPaisComponent {
     this.paisService.buscarPais(this.termino).subscribe(
       paises => {
         console.log(paises);
+        this.paises = paises
       }, error => {
         console.log(error);
         this.hayError = true
+        this.paises = []
       }
     )
   }
