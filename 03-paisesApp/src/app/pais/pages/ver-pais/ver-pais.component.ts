599d768c6b639119bb6ee600c1d6994a714df56e
Ricardo Olarte <raop155@hotmail.com>
2021-07-28 19:19:53 -0500

    Ver pais markup & styles

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -11,16 +12,19 @@ import { switchMap } from 'rxjs/operators';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
     .pipe(
-      switchMap(({ id }) => this.paisService.getPaisPorAlpha(id) )
+      switchMap(({ id }) => this.paisService.getPaisPorAlpha(id) ),
+      tap(console.log)
     )
-    .subscribe((resp) => {
-        console.log(resp);
+    .subscribe((pais) => {
+        this.pais = pais;
       }
     )
   }
