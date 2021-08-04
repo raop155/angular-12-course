c1f555db3f728c626d781e024a12a9d61822bc10
Ricardo Olarte <raop155@hotmail.com>
2021-08-04 17:48:20 -0500

    ngClass, class, [class]

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px;
+    }  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ["africa", "americas", "asia", "europe", "oceania"]
+  regionActiva: string = ""
 
   constructor() { }
 
-  ngOnInit(): void {
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva ? 'btn-primary' : 'btn-outline-primary'
   }
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
+
+    // TODO: hacer llamado al servicio
+  }
+
 
 }
