a14fd4b54aed9d879b0373201ab2bcad4d456e0d
Ricardo Olarte <raop155@hotmail.com>
2021-08-09 17:34:48 -0500

    Intall PrimeNg

---
+++ 04-pipesApp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pipesApp';
+  nombre: string = "Ricardo";
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
+}
